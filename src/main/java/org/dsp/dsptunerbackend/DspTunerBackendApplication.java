d9ba44aebe1eabb59f9bb9ea097e3e5567c63eeb
Rainer Schreiber <rainer@evalentis.com>
2022-09-26 16:27:37 +0200

    Added @EnableAsync

--- src/main/java/org/dsp/dsptunerbackend/DspTunerBackendApplication.java
+++ src/main/java/org/dsp/dsptunerbackend/DspTunerBackendApplication.java
@@ -2,8 +2,10 @@ package org.dsp.dsptunerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class DspTunerBackendApplication {
 
     public static void main(String[] args) {
