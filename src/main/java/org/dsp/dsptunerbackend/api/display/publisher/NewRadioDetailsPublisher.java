30f75560048fdd091994d1fe9036908f25a78e4f
Rainer Schreiber <rainer@evalentis.com>
2022-09-26 12:18:27 +0200

    POC of flux streaming now working 100%

--- src/main/java/org/dsp/dsptunerbackend/api/display/publisher/NewRadioDetailsPublisher.java
+++ src/main/java/org/dsp/dsptunerbackend/api/display/publisher/NewRadioDetailsPublisher.java
@@ -1,18 +1,12 @@
 package org.dsp.dsptunerbackend.api.display.publisher;
 
-import org.dsp.dsptunerbackend.model.radiodetails.RadioData;
 import org.dsp.dsptunerbackend.model.radiodetails.RadioDetails;
-import org.dsp.dsptunerbackend.model.radiodetails.RadioSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
 import reactor.util.concurrent.Queues;
-
-import java.math.BigDecimal;
-import java.util.UUID;
 
 @Component
 public class NewRadioDetailsPublisher {
@@ -35,14 +29,14 @@ public class NewRadioDetailsPublisher {
 //
 //    }
 
-    @Scheduled(fixedRate = 3000)
-    public void onNewRadioDetails() {
-        var radioSettings = new RadioSettings(12, true);
-        var radioData = new RadioData(new BigDecimal("45.63"));
-        var radioDetails = new RadioDetails(UUID.randomUUID().toString(), radioSettings, radioData);
-        this.sink.tryEmitNext(radioDetails);
-        LOG.info("NEW RADIO DETAILS ARE EMITTED TO FLUX STREAM");
-    }
+//    @Scheduled(fixedRate = 3000)
+//    public void onNewRadioDetails() {
+//        var radioSettings = new RadioSettings(12, true);
+//        var radioData = new RadioData(new BigDecimal("45.63"));
+//        var radioDetails = new RadioDetails(UUID.randomUUID().toString(), radioSettings, radioData);
+//        this.sink.tryEmitNext(radioDetails);
+//        LOG.info("NEW RADIO DETAILS ARE EMITTED TO FLUX STREAM");
+//    }
 
     public Flux<RadioDetails> getPublisher() {
         return this.sink.asFlux();
